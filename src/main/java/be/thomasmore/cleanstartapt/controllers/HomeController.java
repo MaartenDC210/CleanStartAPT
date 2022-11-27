00eea7fe9afb4f99376bde3a81e26dc6f4eb1d8c
mdecat <maarten.de.cat@sarens.com>
2022-11-27 15:03:42 +0100

    commit

--- src/main/java/be/thomasmore/cleanstartapt/controllers/HomeController.java
+++ src/main/java/be/thomasmore/cleanstartapt/controllers/HomeController.java
@@ -1,16 +1,28 @@
 package be.thomasmore.cleanstartapt.controllers;
 
+import be.thomasmore.cleanstartapt.model.Pub;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 567;
+    private final String[] pubNames = {"Het Anker","'t Schuurke","De Zwaan","De ton"};
+    private final Pub [] pubs = {
+            new Pub(1,"Bobbejaanland", "http://www.bobbejaanland.be", true, false, false, true),
+            new Pub(2,"Plopsa", "http://www.plopsa.be", false, true, true, true),
+            new Pub(3,"Walibi", "http://www.walibi.be", false, false, true, true),
+            new Pub(4,"Legoland", "https://www.legoland.de/en/", false, false, true, true),
+            new Pub(5,"Phantasialand", "https://www.phantasialand.de/en/", false, true, true, true),
+            new Pub(6,"Efteling", "https://www.efteling.nl", false, true, true, true)
+    };
 
     @GetMapping({"/", "/home"})
     public String home(Model model){
@@ -27,18 +39,52 @@ public class HomeController {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDateTime dateToday = LocalDateTime.now();
         LocalDateTime datePlus30Days = dateToday.plusDays(30);
+        Boolean weekend = false;
+        if(dateToday.getDayOfWeek() == DayOfWeek.SATURDAY || dateToday.getDayOfWeek() == DayOfWeek.SUNDAY )
+        {
+            weekend = true;
+        }
+
         model.addAttribute("dateOfToday", dtf.format(dateToday));
         model.addAttribute("datePlus30", dtf.format(datePlus30Days));
+        model.addAttribute("weekend", weekend);
         return "pay";
     }
-    @GetMapping({"/venuedetails","/venuedetails/{venueName}"})
-    public String venueDetails(Model model, @PathVariable(required = false) String venueName){
-        model.addAttribute("venueName", (venueName != null) ? venueName : "No venue chosen");
-        return "venueDetails";
+    @GetMapping({"/pubdetails","/pubdetails/{pubName}"})
+    public String pubDetails(Model model, @PathVariable(required = false) String pubName){
+        model.addAttribute("pubName", pubName );
+        return "pubDetails";
+    }
+    @GetMapping({"/pubdetailsbyindex","/pubdetailsbyindex/","/pubdetailsbyindex/{pubindex}"})
+    public String pubDetailsByIndex(Model model, @PathVariable(required = false) String pubindex){
+        Pub pub = null;
+
+        if(pubindex !=null && Integer.parseInt(pubindex)%1 == 0 && Integer.parseInt(pubindex)>= 0 && Integer.parseInt(pubindex)< pubs.length )
+        {
+            //get pub object
+            pub = pubs[Integer.parseInt(pubindex)];
+        }
+
+        int prevIndex = Integer.parseInt(pubindex)-1;
+        if(prevIndex<0){
+            prevIndex = pubs.length - 1;
+        }
+
+        int nextIndex = Integer.parseInt(pubindex)+1;
+        if(nextIndex >pubs.length-1)
+        {
+            nextIndex = 0;
+        }
+
+        model.addAttribute("pub",pub);
+        model.addAttribute("prevIndex", prevIndex);
+        model.addAttribute("nextIndex", nextIndex);
+        return "pubdetailsbyindex";
     }
 
-    @GetMapping("/venuelist")
-    public String venueList(Model model){
-        return "venuelist";
+    @GetMapping("/publist")
+    public String pubList(Model model){
+        model.addAttribute("pubNames", pubNames);
+        return "publist";
     }
 }
---
+++ src/main/java/be/thomasmore/cleanstartapt/model/Pub.java
@@ -0,0 +1,84 @@
+package be.thomasmore.cleanstartapt.model;
+
+import jakarta.persistence.Id;
+
+public class Pub {
+    @Id
+    private int id;
+    private String PubName;
+    private String linkMoreInfo;
+    private Boolean hasFreeParking;
+    private Boolean toddlerFriendly;
+    private Boolean hasGoodBeer;
+    private Boolean teenagerFriendly;
+
+    public Pub() {
+
+    }
+
+    public Pub(int id, String PubName, String linkMoreInfo, Boolean hasFreeParking, Boolean toddlerFriendly, Boolean hasGoodBeer, Boolean teenagerFriendly) {
+        this.id = id;
+        this.PubName = PubName;
+        this.linkMoreInfo = linkMoreInfo;
+        this.hasFreeParking = hasFreeParking;
+        this.toddlerFriendly = toddlerFriendly;
+        this.hasGoodBeer = hasGoodBeer;
+        this.teenagerFriendly = teenagerFriendly;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPubName() {
+        return PubName;
+    }
+
+    public void setPubName(String PubName) {
+        this.PubName = PubName;
+    }
+
+    public String getLinkMoreInfo() {
+        return linkMoreInfo;
+    }
+
+    public void setLinkMoreInfo(String linkMoreInfo) {
+        this.linkMoreInfo = linkMoreInfo;
+    }
+
+    public Boolean getHasFreeParking() {
+        return hasFreeParking;
+    }
+
+    public void setHasFreeParking(Boolean hasFreeParking) {
+        this.hasFreeParking = hasFreeParking;
+    }
+
+    public Boolean getToddlerFriendly() {
+        return toddlerFriendly;
+    }
+
+    public void setToddlerFriendly(Boolean toddlerFriendly) {
+        toddlerFriendly = toddlerFriendly;
+    }
+
+    public Boolean getHasGoodFood() {
+        return hasGoodBeer;
+    }
+
+    public void setHasGoodFood(Boolean hasGoodFood) {
+        this.hasGoodBeer = hasGoodFood;
+    }
+
+    public Boolean getTeenagerFriendly() {
+        return teenagerFriendly;
+    }
+
+    public void setTeenagerFriendly(Boolean teenagerFriendly) {
+        teenagerFriendly = teenagerFriendly;
+    }
+}
